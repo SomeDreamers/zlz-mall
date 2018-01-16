54a926aa81fbad6f7b6d1284ee1d72f58c49fc9e
hxw <admin@DESKTOP-4CJS7D8>
2018-01-16 12:13:30 +0800

    添加微信接受消息模块

---
+++ src/Pls.Plug/weixin/CustomMessageHandler.cs
@@ -0,0 +1,35 @@
+﻿using Pls.Plug.weixin;
+using Senparc.Weixin.Context;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.MessageHandlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Pls.Plug.Weixin
+{
+    public class CustomMessageHandler : MessageHandler<CustomMessageContext>
+    {
+        public CustomMessageHandler(Stream inputStream, PostModel postModel)
+            : base(inputStream, postModel)
+        {
+
+        }
+
+        public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageText>(); //ResponseMessageText也可以是News等其他类型
+            responseMessage.Content = "默认消息";
+            return responseMessage;
+        }
+
+        public override IResponseMessageBase OnTextRequest(RequestMessageText requestMessage)
+        {
+            var responseMessage = CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = string.Format("您刚才发送了文字信息：{0}", requestMessage.Content);
+            return responseMessage;
+        }
+    }
+}
